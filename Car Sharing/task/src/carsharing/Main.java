08c7772bb6a0b070068c9c5cd58674a552347719
Nick Bauters (7dbb) <nick.bauters@colruytgroup.com>
2024-11-21 15:13:10 +0100

    refactor: Implement Connector in the main class

--- Car Sharing/task/src/carsharing/Main.java
+++ Car Sharing/task/src/carsharing/Main.java
@@ -3,29 +3,34 @@ package carsharing;
 import java.sql.SQLException;
 
 public class Main {
+    public static Connector connector;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         String dbName = "CarSharing_DB";
         if (args.length == 2 && args[0].equals("-databaseFileName")) {
             dbName = args[1];
         }
 
         try (Connector conn = new Connector(dbName)) {
+            connector = conn;
 
-            conn.state("create table COMPANY (ID INT, NAME VARCHAR(255))");
-            conn.state("insert into COMPANY (ID, NAME) values (1, 'Colruyt Group')");
-
-            Thread.sleep(5_000);
-
-            var result = conn.query("select * from COMPANY");
-            result.next();
-            System.out.println(result.getString("NAME"));
-            System.out.println(result.getInt("ID"));
-
-        } catch (SQLException sqle) {
-            System.out.println("SQLException: " + sqle.getMessage());
-        } catch (Exception ie) {
-            System.out.println("InterruptedException: " + ie.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());;
         }
+    }
+
+    public static void createCompanyTable() throws SQLException {
+        connector.state("CREATE TABLE COMPANY(ID INT AUTO_INCREMENT PRIMARY KEY, NAME VARCHAR(255) UNIQUE NOT NULL)");
+    }
+
+    public static void insertCompany(int id, String name) throws SQLException {
+        connector.state("INSERT INTO COMPANY(ID, NAME) VALUES(%d, '%s')".formatted(id, name));
+    }
+
+    public static void selectTestCompany() throws SQLException {
+        var result = connector.query("select * from COMPANY");
+        result.next();
+        System.out.println(result.getString("NAME"));
+        System.out.println(result.getInt("ID"));
     }
 }
